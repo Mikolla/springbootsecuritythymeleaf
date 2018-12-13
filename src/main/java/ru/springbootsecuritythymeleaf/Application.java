0fce29d0c7ffce40d691ea3a9c70b8dc73e2e78e
Mikolla <miqolay@gmail.com>
2018-12-13 18:55:50 +0500

    clean properties, depensies and cfg files

--- src/main/java/ru/springbootsecuritythymeleaf/Application.java
+++ src/main/java/ru/springbootsecuritythymeleaf/Application.java
@@ -5,12 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import ru.springbootsecuritythymeleaf.config.WebConfig;
 import ru.springbootsecuritythymeleaf.config.security.SecurityConfig;
 
 @SpringBootApplication
 @Configuration
-@Import({WebConfig.class,  SecurityConfig.class})
+@Import({ SecurityConfig.class})
 public class Application {
 
     public static void main(String[] args) {
